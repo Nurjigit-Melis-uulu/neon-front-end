84373f70cd8db13ecbbb541fc2b8b95cca75be3e
Nurjigit <45354207+Nurjigit-Melis-uulu@users.noreply.github.com>
2019-06-02 00:24:10 +0600

    add control

---
+++ main.js
@@ -0,0 +1,23 @@
+let button = document.querySelector("#button");
+let lines = document.querySelectorAll(".line");
+let buttonStatus = false;
+
+button.addEventListener("click", () => {
+  check();
+});
+
+function check() {
+  if (buttonStatus) {
+    for (let i = 0; i < lines.length; i++) {
+      lines[i].style.animation = "neon 2s 1 forwards";
+    }
+    button.innerHTML = "stop";
+    buttonStatus = false;
+  } else {
+    for (let i = 0; i < lines.length; i++) {
+      lines[i].style.animation = "neonStop 2s 1 forwards";
+    }
+    button.innerHTML = "play";
+    buttonStatus = true;
+  }
+}
